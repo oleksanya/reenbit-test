fa7191742ae379762030cc8efd0008a9da4310d8
Oleksandra Kryvenko <alexxa.wrk@gmail.com>
2025-05-16 01:06:38 +0300

    add toast notification component, inject socket service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatContainerComponent } from './components/chat-container/chat-container.component';
+import { ToastComponent } from './components/toast/toast.component';
+import { SocketService } from './services/socket.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatContainerComponent],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Test Chat App';
   http = inject(HttpClient);
+  private socketService = inject(SocketService);
 
-  constructor() {}
+  constructor() {
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      this.socketService.connect(token);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.socketService.disconnect();
+  }
 }
